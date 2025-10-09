46e9a4ab574036c086bfd737c69d8628bef917f9
Ubuntu <ubuntu@ip-172-31-89-47.ec2.internal>
2025-10-09 22:29:07 +0000

    added deploy.sh

--- deployment_lambda/deploy.sh
+++ deployment_lambda/deploy.sh
@@ -1,16 +1,22 @@
-IMAGE_NAME ="churn_prediction_lambda"
-ECR_URl =
-REPO_URL =${ECR_URL}/churn_prediction_lambda
+IMAGE_NAME ="churn-prediction-lambda"
 AWS_REGION = "us-east-1"
-REMOTE_IMAGE_TAG = "${ECR_URL}/churn_prediction_lambda:v1"
+AWS_ACCOUNT_ID=$(aws sts get-caller-identity | jq -r ".Account")
+
+# Get latest commit SHA and current datetime
+COMMIT_SHA=$(git rev-parse --short HEAD)
+DATETIME=$(date +"%Y%m%d-%H%M%S")
+IMAGE_TAG="${COMMIT_SHA}-${DATETIME}"
+
+ECR_URI="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com"
+IMAGE_URI="${ECR_URI}/${IMAGE_NAME}:${IMAGE_TAG}"
 
 #LOGIN TO ECR
 aws ecr get-login-password \
     --region ${AWS_REGION} | docker login \
     --username AWS \
-    --password-stdin ${ECR_URL}
+    --password-stdin ${ECR_URI}
 
-docker build -t ${IMAGE_NAME} .
-docker tag ${IMAGE_NAME} ${REMOTE_IMAGE_TAG}
-docker push ${REMOTE_IMAGE_TAG}
+docker build -t ${IMAGE_NAME}:${IMAGE_TAG} .
+docker tag ${IMAGE_NAME}:${IMAGE_TAG} ${IMAGE_URI}
+docker push ${IMAGE_URI}
 echo "Image pushed to ECR successfully"
