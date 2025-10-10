ac567e7009390b18db7ff758560b43d0ad1c4482
Ubuntu <ubuntu@ip-172-31-89-47.ec2.internal>
2025-10-10 20:48:24 +0000

    added deploy.sh

--- deployment_lambda/deploy.sh
+++ deployment_lambda/deploy.sh
@@ -1,5 +1,6 @@
-IMAGE_NAME ="churn-prediction-lambda"
-AWS_REGION = "us-east-1"
+IMAGE_NAME="churn-prediction-lambda"
+AWS_REGION="us-east-1"
+
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity | jq -r ".Account")
 
 # Get latest commit SHA and current datetime
@@ -10,13 +11,22 @@ IMAGE_TAG="${COMMIT_SHA}-${DATETIME}"
 ECR_URI="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com"
 IMAGE_URI="${ECR_URI}/${IMAGE_NAME}:${IMAGE_TAG}"
 
-#LOGIN TO ECR
+#run it only once
+# aws ecr create-repository \
+#   --repository-name ${IMAGE_NAME} \
+#   --region ${AWS_REGION}
+
 aws ecr get-login-password \
-    --region ${AWS_REGION} | docker login \
-    --username AWS \
-    --password-stdin ${ECR_URI}
+  --region ${AWS_REGION} \
+| docker login \
+  --username AWS \
+  --password-stdin ${ECR_URI}
 
 docker build -t ${IMAGE_NAME}:${IMAGE_TAG} .
 docker tag ${IMAGE_NAME}:${IMAGE_TAG} ${IMAGE_URI}
 docker push ${IMAGE_URI}
-echo "Image pushed to ECR successfully"
+
+aws lambda update-function-code \
+  --function-name churn-prediction-docker \
+  --image-uri ${IMAGE_URI} \
+  --region ${AWS_REGION}
